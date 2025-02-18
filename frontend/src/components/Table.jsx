3a069257da23dcae6aa5ec6f8b7f349f8d78fcde
Erick Guarionex <erickguarionexdev@gmail.com>
2025-02-18 03:27:14 -0400

    Adicion de eliminar fila de tabla

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -15,33 +15,25 @@ import EditIcon from "@mui/icons-material/Edit";
 const normalizeKey = (str) =>
   str.toLowerCase().replace(/\s+/g, "").replace(/[^\w]/g, "");
 
-export default function ColumnGroupingTable({ columnas, data }) {
+export default function ColumnGroupingTable({
+  columnas,
+  data,
+  setIsModalConfirmacion,
+}) {
   const columnMap = columnas.reduce((acc, col) => {
     acc[normalizeKey(col)] = col;
     return acc;
   }, {});
 
   const columns = [
-    { id: "id", label: "ID", minWidth: 50 },
-    ...columnas.map((col) => ({
+    ...columnas.map((col, i) => ({
       id: normalizeKey(col),
       label: col,
-      minWidth: 100,
+      minWidth: i === 0 ? 40 : 100,
       align: "left",
     })),
     { id: "acciones", label: "Acciones", minWidth: 100, align: "center" },
   ];
-
-  const normalizedRows = data.map((row, index) => {
-    let newRow = { id: index + 1 }; //ID GENERADO
-    Object.keys(row).forEach((key) => {
-      const normalizedKey = normalizeKey(key);
-      if (columnMap[normalizedKey]) {
-        newRow[normalizedKey] = row[key];
-      }
-    });
-    return newRow;
-  });
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -55,18 +47,13 @@ export default function ColumnGroupingTable({ columnas, data }) {
     setPage(0);
   };
 
-  // MANEJAR LOS BOTONES
-  const handleEditar = (id) => {
-    console.log(`Editar fila con ID: ${id}`);
-  };
-
-  const handleEliminar = (id) => {
-    console.log(`Eliminar fila con ID: ${id}`);
+  const handleEditar = (index) => {
+    console.log(`Editar fila en índice: ${index}`);
   };
 
   return (
     <Paper sx={{ width: "95%" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ maxHeight: 420 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableRow>
             {columns.map((column) => (
@@ -85,10 +72,10 @@ export default function ColumnGroupingTable({ columnas, data }) {
             ))}
           </TableRow>
           <TableBody>
-            {normalizedRows
+            {data
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+              .map((row, index) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={index}>
                   {columns.map((column) => {
                     if (column.id === "acciones") {
                       return (
@@ -97,25 +84,23 @@ export default function ColumnGroupingTable({ columnas, data }) {
                             variant="contained"
                             color="warning"
                             size="small"
-                            onClick={() => handleEditar(row.id)}
+                            onClick={() => handleEditar(index)}
                             startIcon={<EditIcon />}
-                            style={{
-                              marginRight: "5px",
-                              textTransform: "capitalize",
-                            }}
-                          >
-                            Editar
-                          </Button>
+                            style={{ marginRight: "5px", paddingRight: 0 }}
+                          />
                           <Button
                             variant="contained"
                             color="error"
                             size="small"
-                            onClick={() => handleEliminar(row.id)}
+                            onClick={() =>
+                              setIsModalConfirmacion({
+                                active: true,
+                                id: row.id,
+                              })
+                            }
                             startIcon={<DeleteIcon />}
-                            style={{ textTransform: "capitalize" }}
-                          >
-                            Eliminar
-                          </Button>
+                            style={{ paddingRight: 0 }}
+                          />
                         </TableCell>
                       );
                     }
@@ -135,7 +120,7 @@ export default function ColumnGroupingTable({ columnas, data }) {
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
-        count={normalizedRows.length}
+        count={data.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
--- frontend/src/pages/Factura.jsx
+++ frontend/src/pages/Factura.jsx
@@ -4,26 +4,75 @@ import HeaderGroup from "../components/HeaderGroup";
 import Modal from "../components/Modal";
 import { Button } from "@mui/material";
 import { useState } from "react";
+import Alert from "@mui/joy/Alert";
 import CamposFactura from "../components/CamposFactura";
+import ModalConfirmacion from "../components/ModalConfirmacion";
 
 const columnas = [
+  "ID",
   "Nombre",
   "Descripcion",
   "Fecha Creacion",
-  "Fecha Vencimientos",
+  "Fecha Vencimiento",
 ];
-const rows = [];
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+const fechaCreacion = `${year}-${month}-${day}`;
 
 export default function Factura() {
   const [isModal, setIsModal] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [isModalConfirmacion, setIsModalConfirmacion] = useState({
+    active: false,
+    id: null,
+  });
 
+  const [isError, setIsError] = useState(false);
+  const [mensajeAlerta, setMensajeAlerta] = useState("");
+  const [id, setId] = useState(1);
+
+  const [rows, setRows] = useState([]);
   const [nombre, setNombre] = useState("");
   const [descripcion, setDescripcion] = useState("");
-  const [FechaVencimiento, setFechaVencimiento] = useState("");
+  const [fechaVencimiento, setFechaVencimiento] = useState("");
 
   function showModal() {
     setIsModal(true);
+  }
+
+  function validarInformacion() {
+    if ([nombre, descripcion, fechaVencimiento].includes("")) {
+      setMensajeAlerta("Campos no pueden ir vacios");
+      setIsError(true);
+
+      //DEVOLVER A LA NORMALIDAD
+      setTimeout(() => {
+        setMensajeAlerta("");
+        setIsError(false);
+      }, 3000);
+      return;
+    }
+
+    {
+      /*CERRAR MODAL Y LLENAR FILA*/
+    }
+    setIsModal(false);
+    setNombre("");
+    setDescripcion("");
+    setFechaVencimiento("");
+
+    setRows((rows) => [
+      { id: id, nombre, descripcion, fechaCreacion, fechaVencimiento },
+      ...rows,
+    ]);
+    setId((id) => id + 1);
+  }
+
+  function eliminarElemento(index) {
+    setRows(rows.filter((e, i) => e.id !== index));
   }
 
   return (
@@ -34,12 +83,25 @@ export default function Factura() {
       </header>
 
       <main>
-        <Table columnas={columnas} data={rows} />
+        <Table
+          columnas={columnas}
+          data={rows}
+          onEliminarElemento={eliminarElemento}
+          setIsModalConfirmacion={setIsModalConfirmacion}
+        />
       </main>
 
       {/* MODAL AGREGAR*/}
       {isModal ? (
         <Modal setIsModal={setIsModal} modalNombre="Factura">
+          {mensajeAlerta ? (
+            <div>
+              <Alert color="danger">{mensajeAlerta}</Alert>
+            </div>
+          ) : (
+            ""
+          )}
+
           <CamposFactura
             setNombre={setNombre}
             setDescripcion={setDescripcion}
@@ -49,10 +111,21 @@ export default function Factura() {
             style={{ marginTop: "10px" }}
             variant="contained"
             color="success"
+            onClick={validarInformacion}
           >
             Agregar Factura
           </Button>
         </Modal>
+      ) : (
+        ""
+      )}
+
+      {isModalConfirmacion.active ? (
+        <ModalConfirmacion
+          onEliminarElemento={eliminarElemento}
+          setIsModalConfirmacion={setIsModalConfirmacion}
+          id={isModalConfirmacion.id}
+        />
       ) : (
         ""
       )}
