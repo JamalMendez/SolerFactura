039ad2ae7fddf68646bc1af57700ca8578adc8f4
Erick Guarionex <erickguarionexdev@gmail.com>
2025-02-23 17:42:36 -0400

    Creacion de draft de agregar factura

---
+++ frontend/src/pages/CreacionFactura.jsx
@@ -0,0 +1,15 @@
+export default function CreacionFactura(){
+    return(
+        <>
+           <div className="creacion-factura-contenedor">
+                <main className="inputs-factura">
+                    <h1>CREACION FACTURA</h1>
+                </main>
+
+                <aside className="visualizacion-factura">
+                    <h1>FACTURA VISUALIZACION</h1>
+                </aside>
+           </div>
+        </>
+    )
+}
